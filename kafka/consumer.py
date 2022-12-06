821a3e81d2c54b6a4ff3dc86058275e6c289114b
Yacine_Ubuntu <yacine.anane@epita.fr>
2022-12-06 11:20:26 +0100

    Consumer prediction + data saving

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,6 +1,11 @@
 from kafka import KafkaConsumer
 import numpy as np
+import pandas as pd
 import json
+import joblib
+
+loaded_model = joblib.load("random_forest.joblib")
+prod_data = []
 
 topic = "view-count_producer"
 consumer = KafkaConsumer(
@@ -10,4 +15,10 @@ consumer = KafkaConsumer(
 
 for msg in consumer:
     data = msg.value["X"]
-    print(np.array(data).flatten())
+    if len(data) != 5:
+        print("Data shape is incorrect. Expected 5, Got", len(data))
+        continue
+
+    print("Prediction:", np.array(loaded_model.predict(data)))
+    prod_data.append(data)
+    pd.DataFrame(prod_data).to_csv("../data/prod_data.csv", mode='a', index=False, header=False)
