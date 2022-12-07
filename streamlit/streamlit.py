178e2220140d6061dbf2cabbc57c4a06fcf02f46
Yacine_Ubuntu <yacine.anane@epita.fr>
2022-12-07 11:37:15 +0100

    Streamlit table from csv

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -1,57 +1,49 @@
+import time
 import pandas as pd
 import streamlit as st
-from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid import AgGrid, GridOptionsBuilder, AgGridTheme
 from st_aggrid.shared import GridUpdateMode
 
-def aggrid_interactive_table(df: pd.DataFrame):
-    """Creates an st-aggrid interactive table based on a dataframe.
-    Args:
-        df (pd.DataFrame]): Source dataframe
-    Returns:
-        dict: The selected row
-    """
-    '''
-    options = GridOptionsBuilder.from_dataframe(
-        df, enableRowGroup=True, enableValue=True, enablePivot=True
-    )
-    
-    options.configure_side_bar()
+# def aggrid_table(df: pd.DataFrame):
+#     """Creates an st-aggrid interactive table based on a dataframe.
+#     Args:
+#         df (pd.DataFrame]): Source dataframe
+#     Returns:
+#         dict: The selected row
+#     """
 
-    options.configure_selection("single")
-    ''' 
-    selection = AgGrid(
-        df,
-        enable_enterprise_modules=True,
-   #     gridOptions=options.build(),
-   #     theme="light",
-        update_mode=GridUpdateMode.MODEL_CHANGED,
-        allow_unsafe_jscode=True,
-    )
+#     gd = GridOptionsBuilder.from_dataframe(df)
+#     gd.configure_pagination(enabled=True)
+#     gd.configure_default_column(editable=True, groupable=True)
 
-    return selection
+#     gridoptions = gd.build()
+#     grid_table = AgGrid(df, gridOptions=gridoptions,
+#                     # update_mode=GridUpdateMode.SELECTION_CHANGED | GridUpdateMode.VALUE_CHANGED,
+#                     update_mode=GridUpdateMode.MODEL_CHANGED,
+#                     height=500,
+#                     allow_unsafe_jscode=True,
+#                     # enable_enterprise_modules = True,
+#                     reload_data=False,
+#                     theme=AgGridTheme.ALPINE)
+
+#     return grid_table
 
 
-st.set_page_config(
-    layout="centered", page_icon="🖱️", page_title="Interactive table app"
-)
-st.title("🖱️ Interactive table app")
-st.write(
-    """This app shows how you can use the [streamlit-aggrid](STREAMLIT_AGGRID_URL) 
-    Streamlit component in an interactive way so as to display additional content 
-    based on user click."""
-)
+# st.set_page_config(
+#     layout="centered", page_icon="🖱️", page_title="View count predictions"
+# )
+# st.title("🖱️ View count predictions")
+# st.write(
+#     """This app displays youtube vidos view count predictions"""
+# )
 
-
-st.write("Go ahead, click on a row in the table below!")
+# Update grid
 data = pd.read_csv("../data/train.csv")
-df = data.iloc[0,:]
-for i in range(1, len(data)):
-    selection = aggrid_interactive_table(df)
-    if selection:
-        st.write("You selected:")
-        st.json(selection["selected_rows"])
+data = data.astype(float)
 
-    new_line = data.iloc[i,:]
-    df = df.append({df.columns[j]:new_line[j] for j in range(len(df.columns))})
-
-    
+df = data.iloc[:3,:]
+table = st.table(df)
+for i in range(len(df), len(data)):
+    new_line = pd.DataFrame([data.iloc[i,:].values], columns=df.columns)
+    table.add_rows(new_line)
+    time.sleep(1)
