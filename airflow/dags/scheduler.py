64de0356a36da9f7ab706bc35079069b8847aa4d
JikanS5 <charli.deluca@yahoo.fr>
2022-12-13 23:07:05 +0100

    change the path of the output file

--- airflow/dags/scheduler.py
+++ airflow/dags/scheduler.py
@@ -19,30 +19,9 @@ default_args = {
 		'retries': 3,
 		'retry_delay': timedelta(minutes=5)
 }
-'''
-hello_world_dag = DAG('hello_world_dag',
-		default_args=default_args,
-		description='Hello World DAG',
-		schedule_interval='* * * * *', 
-		catchup=False,
-		tags=['example, helloworld']
-)
-
-def print_hello():
-		return 'Hello World!'
-
-start_task = DummyOperator(task_id='start_task', dag=hello_world_dag):
-
-hello_world_task = PythonOperator(task_id='hello_world_task', python_callable=print_hello, dag=hello_world_dag)
-
-end_task = DummyOperator(task_id='end_task', dag=hello_world_dag)
-
-start_task >> hello_world_task >> end_task
-'''
 
 
-
-def check_data_drift(output_file="report.html", title="Data drift report"):
+def check_data_drift(output_file="~/Bureau/mlops/report.html", title="Data drift report"):
   sd = SmartDrift(
     pd.read_csv('~/Bureau/mlops/data/random_data.csv'),
     pd.read_csv('~/Bureau/mlops/data/train.csv'),
